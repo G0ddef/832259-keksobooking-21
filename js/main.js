4d715d160f02d94fef3b59618cc49945079d0cf2
Anton <goddef@mail.ru>
2020-10-01 00:05:09 +0300

    Исправил ошибки линтера

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-`use strict`;
+'use strict';
 
 const PINS_COUNT = 8;
 const ROOMS = 6;
@@ -24,30 +24,30 @@ const Coordinates = {
     min: -pinSize,
     max: map.offsetHeight - pinSize
   }
-}
+};
 const getRandomNumber = (rand) => {
   return Math.floor(Math.random() * Math.floor(rand));
-}
+};
 
 const getRandomLocation = (min, max) => {
   return Math.floor(min + Math.random() * (max + 1 - min));
-}
+};
 
 const createPhotosList = (photo) => {
   let photosList = [];
 
   for (let i = 1; i < photo; i++) {
     photosList[i] = `http://o0.github.io/assets/images/tokyo/hotel` + i + `.jpg`;
-  }
+  };
 
   return photosList;
-}
+};
 
 const createAds = (count) => {
   let ads = [];
 
   for (let i = 1; i <= count; i++) {
-    ads.push ({
+    ads.push({
       author: {
         avatar: `img/avatars/user0` + i + `.png`
       },
@@ -71,38 +71,38 @@ const createAds = (count) => {
         x: getRandomLocation(Coordinates.x.min, Coordinates.x.max),
         y: getRandomLocation(Coordinates.y.min, Coordinates.y.max)
       }
-    })
+    });
   }
 
   return ads;
-}
+};
 
 const pinsCreate = (object) => {
   const pinTemplate = mapPin.cloneNode(true);
   const img = pinTemplate.querySelector('img');
-  pinTemplate.style.left=`${object.location.x}px`;
-  pinTemplate.style.top=`${object.location.y}px`;
+  pinTemplate.style.left = `${object.location.x}px`;
+  pinTemplate.style.top = `${object.location.y}px`;
   img.src = object.author.avatar;
   img.alt = object.offer.title;
 
   return pinTemplate;
-}
+};
 
 const pinsFragment = (pinsData) => {
   const fragment = document.createDocumentFragment();
 
   for (let i = 0; i < pinsData.length; i++) {
     fragment.appendChild(pinsCreate(pinsData[i]));
-  }
+  };
 
   return fragment;
-}
+};
 
 const pinsScreen = () => {
   const pinsDataArr = createAds(PINS_COUNT);
   const pinsFragmentNodes = pinsFragment(pinsDataArr);
 
   mapPins.appendChild(pinsFragmentNodes);
-}
+};
 
 pinsScreen();
